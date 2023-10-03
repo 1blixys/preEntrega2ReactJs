29a6ea248db590494d3f99c3293a635cef82d2ed
Fabricio <romerofabriciohxh@gmail.com>
2023-10-02 23:49:32 -0300

    agrego el proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { ItemListContainer } from './components/ItemListContainer';
+import { ItemDetailContainer } from './components/ItemDetailContainer';
+import { NavBar } from './components/NavBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path='/home' element={<ItemListContainer greeting="Bienvenido!" />} />
+        <Route path='/:id' element={<ItemListContainer greeting="Bienvenido!"/>} />
+        <Route path='item/:id' element={<ItemDetailContainer/>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
